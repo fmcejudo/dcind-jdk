026db17c0f66b491c9e282b0294a56c26fd3dd1e
Francisco Cejudo Gómez <cejudogomezf@ryanair.com>
2019-04-12 14:39:05 +0200

    Changing entry point and everything

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM maven:3.6.0-jdk-12-alpine
 MAINTAINER Francisco Miguel Cejudo <fmcejudo@gmail.com>
 
 USER root
-RUN apk add --no-cache \
+RUN apk add --no-cache shadow \
 		ca-certificates
 
 # set up nsswitch.conf for Go's "netgo" implementation (which Docker explicitly uses)
@@ -18,38 +18,9 @@ ENV DOCKER_VERSION 18.09.4
 # https://github.com/docker/docker-ce/blob/5b073ee2cf564edee5adca05eee574142f7627bb/components/packaging/static/hash_files !!
 # (no SHA file artifacts on download.docker.com yet as of 2017-06-07 though)
 
-RUN set -eux; \
-	\
-# this "case" statement is generated via "update.sh"
-	apkArch="$(apk --print-arch)"; \
-	case "$apkArch" in \
-		x86_64) dockerArch='x86_64' ;; \
-		armhf) dockerArch='armel' ;; \
-		aarch64) dockerArch='aarch64' ;; \
-		ppc64le) dockerArch='ppc64le' ;; \
-		s390x) dockerArch='s390x' ;; \
-		*) echo >&2 "error: unsupported architecture ($apkArch)"; exit 1 ;;\
-	esac; \
-	\
-	if ! wget -O docker.tgz "https://download.docker.com/linux/static/${DOCKER_CHANNEL}/${dockerArch}/docker-${DOCKER_VERSION}.tgz"; then \
-		echo >&2 "error: failed to download 'docker-${DOCKER_VERSION}' from '${DOCKER_CHANNEL}' for '${dockerArch}'"; \
-		exit 1; \
-	fi; \
-	\
-	tar --extract \
-		--file docker.tgz \
-		--strip-components 1 \
-		--directory /usr/local/bin/ \
-	; \
-	rm docker.tgz; \
-	\
-	dockerd --version; \
-	docker --version
-
-COPY modprobe.sh /usr/local/bin/modprobe
 COPY docker-entrypoint.sh /usr/local/bin/
-VOLUME /var/run/docker.sock /var/run/docker.sock
-RUN chmod 755 /var/run/docker.sock 
 
-ENTRYPOINT ["docker-entrypoint.sh"]
+EXPOSE 2375 
+
+ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
 CMD ["sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,51 +1,16 @@
 #!/bin/sh
-set -e
+wget -O docker.tgz "https://download.docker.com/linux/static/stable/x86_64/docker-18.09.4.tgz" && \
+   tar --extract \
+    --file docker.tgz \
+    --strip-components 1 \
+    --directory /usr/local/bin/ \
+  ; \
+  rm docker.tgz; \
+  \
+  dockerd --version; \
+  docker --version
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- docker "$@"
-fi
+groupadd docker
+usermod -aG docker $(whoami)
 
-# if our command is a valid Docker subcommand, let's invoke it through Docker instead
-# (this allows for "docker run docker ps", etc)
-if docker help "$1" > /dev/null 2>&1; then
-	set -- docker "$@"
-fi
-
-# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
-if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
-	export DOCKER_HOST='tcp://docker:2375'
-fi
-
-if [ "$1" = 'dockerd' ]; then
-	cat >&2 <<-'EOW'
-
-		📎 Hey there!  It looks like you're trying to run a Docker daemon.
-
-		   You probably should use the "dind" image variant instead, something like:
-
-		     docker run --privileged --name some-overlay-docker -d docker:stable-dind --storage-driver=overlay
-
-		   See https://hub.docker.com/_/docker/ for more documentation and usage examples.
-
-	EOW
-	sleep 3
-fi
-
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-	# add our default arguments
-	set -- dockerd \
-		--host=unix:///var/run/docker.sock \
-		--host=tcp://0.0.0.0:2375 \
-		"$@"
-fi
-
-if [ "$1" = 'dockerd' ]; then
-	# if we're running Docker, let's pipe through dind
-	set -- "$(which dind)" "$@"
-
-	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
-	find /run /var/run -iname 'docker*.pid' -delete
-fi
-
-exec "$@"
+dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 
